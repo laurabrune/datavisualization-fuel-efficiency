f4213cb97dcb31fa23ad73e42139b4e6e20ca018
Laura Brune <laurabrune2802@gmail.com>
2026-04-22 22:16:30 +0200

    Output

--- R/main.R
+++ R/main.R
@@ -26,4 +26,10 @@ here()
 source(here("R", "01_transform_data.R"))
 source(here("R", "02_data_visualization.R"))
 
-
+# save all visualizations in the output folder 
+ggsave(filename = "time.png", path = here("output"), f_time)
+ggsave(filename = "automatic_year_total.png", path = here("output"), f_automatic_year_total)
+ggsave(filename = "nGears_total.png", path = here("output"), f_nGears_total)
+ggsave(filename = "nGears_annual_total.png", path = here("output"), f_nGears_annual_total)
+ggsave(filename = "Cylinders.png", path = here("output"), f_Cylinders)
+ggsave(filename = "make.png", path = here("output"), f_make)
